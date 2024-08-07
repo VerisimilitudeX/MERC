3600f90bdf469baa7e5ad04be5fde5769fbaab35
Piyush Acharya <acharyapiyush1@outlook.com>
2024-08-07 12:53:23 -0700

    add full downloader

---
+++ utilities/download_full.ps1
@@ -0,0 +1,47 @@
+$baseUrl = "http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/"
+$outputPath = "C:\Users\achar\Downloads\RoadmapEpigenomics_Full"
+$visitedUrls = New-Object System.Collections.Generic.HashSet[string]
+$totalSize = 0
+$fileCount = 0
+$lastReportTime = Get-Date
+
+function Get-FilesRecursively($url) {
+    if (-not $visitedUrls.Add($url)) {
+        return
+    }
+    
+    Write-Host "Scanning URL: $url"
+    
+    try {
+        $response = Invoke-WebRequest -Uri $url -UseBasicParsing
+        $links = $response.Links | Where-Object { $_.href -like "*.bigwig" -or $_.href -like "*/" }
+
+        foreach ($link in $links) {
+            $newUrl = [System.Uri]::new([System.Uri]$url, $link.href).AbsoluteUri
+            if ($newUrl -like "*.bigwig") {
+                $fileSize = Get-RemoteFileSize $newUrl
+                $script:totalSize += $fileSize
+                $script:fileCount++
+                Download-File $newUrl
+                Report-Progress
+            } elseif ($newUrl -like "$baseUrl*") {
+                Get-FilesRecursively $newUrl
+            }
+        }
+    }
+    catch {
+        Write-Host "Error accessing $url : $_"
+    }
+}
+
+# The rest of the functions (Get-RemoteFileSize, Download-File, Format-FileSize, Report-Progress) 
+# remain the same as in the subset script
+
+# Start the recursive search
+Get-FilesRecursively $baseUrl
+
+# Final report
+$formattedSize = Format-FileSize $totalSize
+Write-Host "Final Report:"
+Write-Host "Total .bigwig files downloaded: $fileCount"
+Write-Host "Total size of all files: $formattedSize"
