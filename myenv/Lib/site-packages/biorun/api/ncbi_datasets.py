d768ce90caa2926caa8af4a24b8dd6ce2a2af496
Piyush Acharya <acharyapiyush1@outlook.com>
2024-08-16 13:26:46 -0700

    add packages

---
+++ myenv/Lib/site-packages/biorun/api/ncbi_datasets.py
@@ -0,0 +1,6 @@
+"""
+Wrappers to interact with NCBI datasets API.
+"""
+
+DS_REPORT = 'https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCA_000001405/dataset_report'
+
---
+++ myenv/Lib/site-packages/biorun/tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present Istvan Albert <istvan.albert@gmail.com>
+#
+# SPDX-License-Identifier: MIT
